e22bcf8f95986207f288c4a514daf2a0a362d3fd
Cengizhan Yalçın <cengizhanylc@gmail.com>
2026-02-15 17:58:08 +0300

    Add files via upload
    c# ile büfe sistemim

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace sinema_büfe_sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_misir = new System.Windows.Forms.TextBox();
+            this.textBox_cay = new System.Windows.Forms.TextBox();
+            this.textBox_su = new System.Windows.Forms.TextBox();
+            this.textBox_bilet = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label_tutar = new System.Windows.Forms.Label();
+            this.button_hesapla = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.textBox_bilet);
+            this.groupBox1.Controls.Add(this.textBox_su);
+            this.groupBox1.Controls.Add(this.textBox_cay);
+            this.groupBox1.Controls.Add(this.textBox_misir);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 299);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ürün adetleri";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "mısır :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "su :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "çay :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "bilet :";
+            // 
+            // textBox_misir
+            // 
+            this.textBox_misir.Location = new System.Drawing.Point(116, 44);
+            this.textBox_misir.Name = "textBox_misir";
+            this.textBox_misir.Size = new System.Drawing.Size(128, 22);
+            this.textBox_misir.TabIndex = 4;
+            // 
+            // textBox_cay
+            // 
+            this.textBox_cay.Location = new System.Drawing.Point(116, 143);
+            this.textBox_cay.Name = "textBox_cay";
+            this.textBox_cay.Size = new System.Drawing.Size(128, 22);
+            this.textBox_cay.TabIndex = 5;
+            // 
+            // textBox_su
+            // 
+            this.textBox_su.Location = new System.Drawing.Point(116, 94);
+            this.textBox_su.Name = "textBox_su";
+            this.textBox_su.Size = new System.Drawing.Size(128, 22);
+            this.textBox_su.TabIndex = 6;
+            // 
+            // textBox_bilet
+            // 
+            this.textBox_bilet.Location = new System.Drawing.Point(116, 188);
+            this.textBox_bilet.Name = "textBox_bilet";
+            this.textBox_bilet.Size = new System.Drawing.Size(128, 22);
+            this.textBox_bilet.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(354, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 299);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "fiyat listesi";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "mısır : 40TL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "çay : 15TL";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(75, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "bilet : 100TL";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(88, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 16);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "su : 5TL";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Green;
+            this.groupBox3.Controls.Add(this.button_hesapla);
+            this.groupBox3.Controls.Add(this.label_tutar);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Location = new System.Drawing.Point(644, 22);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(270, 299);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(37, 112);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 16);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "TUTAR = ";
+            // 
+            // label_tutar
+            // 
+            this.label_tutar.AutoSize = true;
+            this.label_tutar.Location = new System.Drawing.Point(127, 112);
+            this.label_tutar.Name = "label_tutar";
+            this.label_tutar.Size = new System.Drawing.Size(47, 16);
+            this.label_tutar.TabIndex = 1;
+            this.label_tutar.Text = "0.00TL";
+            this.label_tutar.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // button_hesapla
+            // 
+            this.button_hesapla.Location = new System.Drawing.Point(69, 188);
+            this.button_hesapla.Name = "button_hesapla";
+            this.button_hesapla.Size = new System.Drawing.Size(122, 51);
+            this.button_hesapla.TabIndex = 2;
+            this.button_hesapla.Text = "HESAPLA";
+            this.button_hesapla.UseVisualStyleBackColor = true;
+            this.button_hesapla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_bilet;
+        private System.Windows.Forms.TextBox textBox_su;
+        private System.Windows.Forms.TextBox textBox_cay;
+        private System.Windows.Forms.TextBox textBox_misir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label_tutar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button_hesapla;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinema_büfe_sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int misir, su, cay, bilet, misir_adet, su_adet, cay_adet, bilet_adet, tutar;
+            misir = 40;
+            su = 5;
+            cay = 15;
+            bilet = 100;
+            //misir_adet =Convert.ToInt32(textBox_misir.Text);
+            //su_adet =Convert.ToInt32(textBox_su.Text);
+            //cay_adet =Convert.ToInt32(textBox_cay.Text);
+            //bilet_adet =Convert.ToInt32(textBox_bilet.Text);
+
+            if (!string.IsNullOrEmpty(textBox_misir.Text))
+                misir_adet = Convert.ToInt32(textBox_misir.Text);
+            else
+                misir_adet = 0;
+
+            if (!string.IsNullOrEmpty(textBox_su.Text))
+                su_adet = Convert.ToInt32(textBox_su.Text);
+            else
+                su_adet = 0;
+
+            if (!string.IsNullOrEmpty(textBox_cay.Text))
+                cay_adet = Convert.ToInt32(textBox_cay.Text);
+            else
+                cay_adet = 0;
+
+            if (!string.IsNullOrEmpty(textBox_bilet.Text))
+                bilet_adet = Convert.ToInt32(textBox_bilet.Text);
+            else
+                bilet_adet = 0;
+
+            tutar = (misir_adet*misir) + (su_adet*su_adet) + (cay_adet*cay) + (bilet_adet*bilet);
+            label_tutar.Text = tutar.ToString(tutar + "TL");
+  
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
